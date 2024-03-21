5101b60820c27dbca897a9d22008a596aecf9417
ChulumancoMbete <5100151@mypillar7uwc.ac.za>
2024-03-21 16:34:19 +0200

    Cart Changes

--- Assignment8/FruitDetailsPage.xaml.cs
+++ Assignment8/FruitDetailsPage.xaml.cs
@@ -1,14 +1,52 @@
 using Assignment8.Models;
 using Assignment8.Services;
+using CloudKit;
 using System;
 
 namespace Assignment8;
 
 public partial class FruitDetailsPage : ContentPage
 {
+    private LocalDatabase _database;
+
     public FruitDetailsPage(FruitItemView item)
 	{
         InitializeComponent();
         BindingContext = item;
     }
+
+    private List<ShoppingCart> Basket = new List<ShoppingCart>();
+
+    private async void CartItemclick(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        var item = button.CommandParameter as FruitItems;
+        //Navigation.PushAsync(new FruitDetailsPage());
+        //LoadItem();
+        if (item != null)//(sender is Button button && button.BindingContext is FruitItems item)
+        {
+            ShoppingCart AlreadyAdded = Basket.FirstOrDefault(i => i.FruitId == item.FruitId);
+            if (AlreadyAdded != null)
+            {
+                AlreadyAdded.ItemQuantity++;
+                AlreadyAdded.CartPrice += item.FruitPrice;
+            }
+            else
+            {
+                ShoppingCart cartItem = new ShoppingCart()
+                {
+                    CartItemId = item.FruitId,
+                    FruitName = item.FruitName,
+                    ItemQuantity = 1,
+                    CartPrice = item.FruitPrice,
+                    FruitImage = item.FruitImage,
+
+                };
+                Basket.Add(cartItem);
+                _database.AddToDataBase(cartItem);
+                await DisplayAlert("Notice", "Item Added To Cart", "Great");
+            }
+        }
+    }
+
 }
